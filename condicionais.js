6a4229f5d27dbc1120fd5a3ea2d4a394c5ec1ea4
Duda0810 <164265885+Duda0810@users.noreply.github.com>
2024-03-20 21:51:40 -0300

    Add files via upload

---
+++ condicionais.js
@@ -0,0 +1,56 @@
+let idade = 20
+
+if(idade>= 18){
+    console.log('você é adulto!')
+}else if(idade>=13 && idade>=17){
+    console.log('Você é adolescente!')
+}else {
+    console.log('Você é crinça!')
+}
+
+let paisDeOrigem
+
+if (paisDeOrigem === 'Brasil' ){
+    console.log('Você è brasileiro!')
+}
+ else if (paisDeOrigem === 'Russia') {
+    console.log('Você é Russo!')
+}
+ else if (paisDeOrigem === 'Mexico') {
+    console.log('Você é mexicano!')
+} 
+else if (paisDeOrigem === 'Itália'){
+    console.log('Você é italiano')
+}
+else {
+    console.log('Nacionalidade não encontrada')
+}
+
+
+
+let comida = 'pizza'
+let comidaFavorita = 'pizza'
+
+if (comida === comidaFavorita) {
+    console.log('hoje vou janta bem!')
+}
+
+let time = 'grêmio'
+
+if (time === 'grêmio') {
+    console.log('você é gremista!')
+}
+else (console.log("você é colorada!")
+)
+
+const idadePai = 40
+const idadeMae = 40
+
+if (idadePai > idadeMae){
+    console.log('maior')
+}
+else if (idadeMae > idadePai){
+    console.log('menor')
+}
+else 
+    console.log("iguais")
---
+++ switchcase.js
@@ -0,0 +1,33 @@
+let paisDeOrigem
+
+switch(paisDeOrigem){
+    case 'Brasil':
+        console.log('Brasileiro!')
+        break
+    case 'EUA':
+        console.log('Americano')
+        break
+    case 'Inglaterra':
+        console.log('britânico')
+        break
+    default:
+        console.log('nacionalidade nao encontrada')
+        break
+}
+ 
+let pokemon
+
+switch(pokemon){
+    case 'charmander':
+        console.log('fogo')
+        break
+    case 'bulbasauro':
+        console.log('grama e veneno')
+        break
+    case 'squirtle':
+        console.log('agua')
+        break
+    default:
+        console.log('pokemon nao escolhido')
+        break
+}
