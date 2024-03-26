4e090c0633928b338bcb8854cae2f6c4e33a4734
Duda0810 <164265885+Duda0810@users.noreply.github.com>
2024-03-25 21:50:02 -0300

    Add files via upload

---
+++ Aula 25-03 Prof Lucas/aulalucas.js
@@ -0,0 +1,31 @@
+
+let a = 15; 
+let b = 5;  
+let c = 15;
+let d = 10; 
+let resultado
+
+
+if ((a > 10 && a < 20) || b === 5) {
+    console.log(resultado);
+}
+
+
+if (a > 10 || b === 5) {
+    console.log(resultado);
+}
+
+
+if ((a > 10 && a < 20) || b === 5 || c === 10) {
+    console.log(resultado);
+}
+
+
+if ((a === 10 || b === 5) && !(c === 10)) {
+    console.log(resultado);
+}
+
+
+if ((a > 10 || b === 5) && !(c === 10) && !(d === 20)) {
+    console.log(resultado);
+}
